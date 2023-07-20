f7af1a9c91861a0520ad4b2318e5069754ae27d8
parthbpatel <parthtech3010@gmail.com>
2023-07-20 19:03:55 +0530

    Display posts on the home page - Generate Posts controller and create an index action.   Inside the index action retrieve Post records - Declare routes for posts - Create a _post.html.erb partial inside posts directory - Render posts inside the home page's main content

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
   
   root "pages#index"
 end
