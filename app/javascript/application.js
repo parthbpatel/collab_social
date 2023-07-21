548ed352166baac15e72c28c803c1d3801f4ace0
parthbpatel <parthtech3010@gmail.com>
2023-07-21 10:52:09 +0530

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "posts/style"
 
 //= require jquery
+//= require jquery_ujs
+//= require bootstrap/dropdown
 //= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
